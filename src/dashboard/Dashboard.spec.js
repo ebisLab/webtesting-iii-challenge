4767be2497c53096f3c922b1b9023351283c3199
ebisLab <ebaiden@techie.com>
2019-08-20 16:11:41 -0400

    display,controls attempted

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import {render} from 'react-testing-library'
+// import {render} from '@testing-library/react'
+
+describe('<Dashboard />', () => {
+    it('renders without crashing',  () => {
+        render(<Dashboard />);
+    });
+    it('default state open and unlocked', () => {
+        const { getByText} = render(<Dashboard />);
+        getByText(/^open$/i);
+        getByText(/^unlocked$/i)
+
+        const lockBtn = getByText(/^lock gates$/i);
+        const closeBtn = getByText(/^close gate$/i);
+        // expect(queryByText(/closed/i)).toBe();
+    })
+})
